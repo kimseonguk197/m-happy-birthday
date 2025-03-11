6dd9bb852ead7bcc8d605053f0073fec8c7194d4
kimseonguk <71623837+kimseonguk197@users.noreply.github.com>
2025-03-11 13:18:40 +0900

    Update Cake.tsx

--- components/Cake.tsx
+++ components/Cake.tsx
@@ -18,8 +18,8 @@ function Cake({ gift }: { gift?: boolean; message?: string }) {
       <div className='w-[350px] h-[100px] bg-gray-100 overflow-hidden relative rounded-sm'>
         <Circle count={8} />
         <div className='w-full flex justify-center mt-6'>
-          <Image src='/yeji.jpg' width={80} height={80} alt='ceo' />
-          <Image src='/yeji.jpg' width={80} height={80} alt='ceo' />
+          <Image src='/jh.png' width={80} height={80} alt='ceo' />
+          <Image src='/js.png' width={80} height={80} alt='ceo' />
         </div>
       </div>
       <div className='w-[450px] h-[100px] bg-gray-100 overflow-hidden relative rounded-sm'>
